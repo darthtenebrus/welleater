a7d82fad34993732904955900677bb151d7f8aec
esorochinskiy <esorochinskiy@iqr.su>
2022-06-01 10:55:16 +0300

    output notifications setup

--- lang/en.lua
+++ lang/en.lua
@@ -1,20 +1,20 @@
 WellEater = WellEater or {}
 local L = {
     generalSetupDescription = "Auto eat your preferred meals provided by your inventory after food or drink buff expiration",
-    foodQualityHeader = "Quality Of Food to search",
+    foodQualityHeader = "Quality of food to search",
     foods = {
         [ITEM_QUALITY_MAGIC] = "Normal (green)",
         [ITEM_QUALITY_ARCANE] = "Excellent (blue)",
         [ITEM_QUALITY_ARTIFACT] = "Artifact (cyan)",
         [ITEM_QUALITY_LEGENDARY] = "Legendary (gold)",
     },
-    timerSetupHeader = "Inventory Scan Timer",
+    timerSetupHeader = "Character status scan timer",
     timerSetupLabel = "Scan period, ms",
-    timerSetupLabel_TT = "How often the inventory is scanned for food. The more the better but more probably " ..
+    timerSetupLabel_TT = "How often the character is scanned for food buffs. The more the better but more probably " ..
             "you can run out of food for a long time in critical situation",
 
     youEat = "You have eaten <<1>>",
-    outputSetupHeader = "Meal Eaten Notification Output",
+    outputSetupHeader = "Meal eaten notification output",
     outputOnScreen = "On screen",
     outputSetupHeader_TT = "When on the notification about the meal eaten is written to the screen not only to the debug log",
 }
